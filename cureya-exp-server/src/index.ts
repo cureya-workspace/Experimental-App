3ade21e507d7891a9982dde1d278c168278b5062
Suyash Lawand (DELL) <suyash.lawand@gmail.com>
2023-09-27 01:34:51 +0530

    reinit: dir structure

--- cureya-exp-server/src/index.ts
+++ cureya-exp-server/src/index.ts
@@ -1,21 +1,24 @@
-import  express, { Request, Response, Express, Application} from 'express'
+import  express, { Request, Response, Express} from 'express'
 import dotenv from 'dotenv';
+import baseRouter from './routers/base.router';
 
 dotenv.config();
 
 // Initialize
 const app: Express = express();
-const PORT: any = process.env.PORT;
 
 // Middlewares
 app.use(express.json());
 
+
+// Router
+app.use(baseRouter);
 
 app.get('/', (req: Request, res: Response) => {
   console.log("first")
   res.send({"hi":"hello"});
 });
 
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${process.env.PORT}`);
 });
